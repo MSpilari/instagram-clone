dde9f88d884d230c9f87a2d8bff553a5d9ba37a9
MSpilari <mb.spilari@hotmail.com>
2021-11-24 17:16:55 -0300

    Componentizando o Story, inserindo os dados da API

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,21 +1,46 @@
-import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { api } from '../services/api'
+import { Story } from './Story'
+
+interface IStories {
+	login: {
+		username: string
+	}
+	picture: {
+		thumbnail: string
+	}
+}
+
+interface IResponse {
+	results: []
+}
 
 const Stories = () => {
+	const [stories, setStories] = useState([] as IStories[])
+
+	useEffect(() => {
+		const hasStories = sessionStorage.getItem('stories')
+
+		hasStories
+			? setStories(JSON.parse(hasStories))
+			: api.get<IResponse>('?results=15').then(response => {
+					const { results } = response.data
+
+					setStories(results)
+					sessionStorage.setItem('stories', JSON.stringify(results))
+			  })
+	}, [])
+
 	return (
-		<section className='flex col-span-2 border-2 border-gray-200 my-2'>
-			<div className='flex flex-col relative h-28 items-center justify-center'>
-				<div className='relative h-16 w-16 border-2  border-red-500 rounded-full overflow-hidden'>
-					<Image
-						src='https://github.com/MSpilari.png'
-						alt='User Image'
-						layout='fill'
-						objectFit='contain'
-					/>
-				</div>
-				<p>MSpilari</p>
-			</div>
+		<section className='flex relative items-center col-span-2 border-2 border-gray-200 m-2 overflow-x-scroll lg:w-10/12 rounded-sm lg:ml-auto space-x-2'>
+			{stories.map((story, index) => (
+				<Story
+					key={index}
+					avatar={story.picture.thumbnail}
+					username={story.login.username}
+				/>
+			))}
 		</section>
 	)
 }
-
 export { Stories }
