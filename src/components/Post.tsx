d94ac75093527d61716652733e552d8a0da8716e
MSpilari <mb.spilari@hotmail.com>
2021-11-30 12:21:40 -0300

    Adicionando o campo de comentários no post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,12 +3,12 @@ import { HiOutlineDotsHorizontal } from 'react-icons/hi'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { FiMessageCircle } from 'react-icons/fi'
 import { FaRegPaperPlane } from 'react-icons/fa'
-import { BsBookmark } from 'react-icons/bs'
+import { BsBookmark, BsEmojiSmile } from 'react-icons/bs'
 import IGPicExample from '../assets/igPhotoExample.jpg'
 
 const Post = () => {
 	return (
-		<div className='border-2 m-2 border-gray-300 rounded-sm lg:mr-5'>
+		<div className='border-2 m-2  border-gray-300 rounded-sm max-w-3xl lg:mr-5 xl:w-11/12 xl:mx-auto '>
 			<div className='flex items-center m-3'>
 				<div className='relative w-12 h-12 rounded-full overflow-hidden mr-2'>
 					<Image
@@ -27,13 +27,14 @@ const Post = () => {
 					<HiOutlineDotsHorizontal />
 				</button>
 			</div>
-			<div className='relative w-full h-96 '>
+			<div className='relative w-full h-96'>
 				<Image
 					src={IGPicExample}
 					alt='Post Image'
 					layout='fill'
 					objectFit='fill'
 					sizes='50vw'
+					priority
 				/>
 			</div>
 			<div className='flex items-center m-2 w-full'>
@@ -46,9 +47,19 @@ const Post = () => {
 				<button className='text-3xl m-1 hover:text-gray-400'>
 					<FaRegPaperPlane />
 				</button>
-				<button className='ml-auto mr-2 text-3xl hover:text-gray-400'>
+				<button className='ml-auto mr-4 text-3xl hover:text-gray-400'>
 					<BsBookmark />
 				</button>
+			</div>
+			<div className='flex items-center m-2'>
+				<button className='text-2xl mx-2 hover:text-gray-400'>
+					<BsEmojiSmile />
+				</button>
+				<textarea
+					className='resize-none w-full mx-1'
+					placeholder='Add a comment...'
+				/>
+				<button className='text-sm text-blue-500 font-bold'>Publish</button>
 			</div>
 		</div>
 	)
