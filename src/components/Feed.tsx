9074234b6fdf6d184cb9baad757b9fb448b7f214
MSpilari <mb.spilari@hotmail.com>
2021-11-30 11:31:46 -0300

    Simulando a entrada de vários posts no Feed

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,21 +1,11 @@
-import Image from 'next/image'
+import { Post } from './Post'
 
 const Feed = () => {
 	return (
-		<section className='flex flex-col col-span-2 col-start-1 row-start-1 w-3/4 ml-auto'>
-			<div className=' border-2 border-gray-300 rounded-sm'>
-				<div>
-					<div className='relative w-12 h-12 rounded-full overflow-hidden'>
-						<Image
-							src='https://github.com/MSpilari.png'
-							alt='User Image'
-							layout='fill'
-							objectFit='contain'
-							sizes='50vw'
-						/>
-					</div>
-				</div>
-			</div>
+		<section className='flex flex-col col-span-2 col-start-1 lg:w-3/4  lg:row-start-1 lg:ml-auto lg:mt-36 '>
+			<Post />
+			<Post />
+			<Post />
 		</section>
 	)
 }
