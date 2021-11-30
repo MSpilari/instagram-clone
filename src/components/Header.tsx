9221ac123ae4aa921622291d00495771dc2d7f96
MSpilari <mb.spilari@hotmail.com>
2021-11-30 11:20:42 -0300

    Deixando o header fixo na página mesmo dando o scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ import Instalogo from '../assets/Instagram_logo.svg'
 
 const Header = () => {
 	return (
-		<header className='h-16 w-full border-b-2 border-gray-200 '>
+		<header className='h-16 w-full border-b-2 border-gray-200 bg-white z-20 sticky top-0'>
 			<div className='h-full w-full flex items-center justify-between lg:w-9/12 lg:mx-auto'>
 				<div className='relative hidden h-full w-28 lg:flex'>
 					<Image
